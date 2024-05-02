069ea91486110f9cce32e44e61aa6eff82eda14c
gaspym97 <angelgasparmiralles@gmail.com>
2024-05-02 10:38:59 -0400

    image added to the index page

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import TypeWriteEffect from './helpers/TypeWriteEffect'
+import Image from 'next/image'
 
 function Hero() {
     return (
@@ -20,10 +21,19 @@ function Hero() {
                             <button className='relative flex h-[50px] w-40 items-center justify-center font-semibold overflow-hidden bg-red-500 text-white shadow-2xl transition-all before:absolute before:h-0 before:w-0 before:rounded-full before:bg-yellow-300 hover:text-black before:duration-500 before:ease-out hover:before:h-56 hover:before:w-56'>
                                 <span className='relative z-10'>Contact Me</span>
                             </button>
-                            <button className='before:ease relative h-12 w-40 overflow-hidden bg-yellow-300 text-black font-semibold shadow-2xl before:absolute before:left-0 before:-ml-2 before:h-48 before:w-48 before:origin-top-right before:-translate-x-full before:translate-y-12 before:-rotate-90 before:bg-green-700 before:transition-all before:duration-300  hover:shadow-black hover:-rotate-180'>
+                            <button className='before:ease relative h-12 w-40 overflow-hidden bg-yellow-300 text-black font-semibold shadow-2xl before:absolute before:left-0 before:-ml-2 before:h-48 before:w-48 before:origin-top-right before:-translate-x-full before:translate-y-12 before:-rotate-90 before:bg-red-500 before:transition-all before:duration-300  hover:shadow-black hover:before:-rotate-180 hover:text-white'>
                                 <span className='relative z-10'>Download CV</span>
                             </button>
                         </div>
+                    </div>
+                    <div className='hidden md:block'>
+                        <Image
+                            src="/images/hero.png"
+                            alt='hero'
+                            width={500}
+                            height={500}
+                            className='object-contain'
+                        />
                     </div>
                 </div>
             </div>
